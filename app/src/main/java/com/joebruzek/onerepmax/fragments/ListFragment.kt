5d4bb0f572452089636b873b6a1abe434afde713
Joe Bruzek <jbruzek@vt.edu>
2019-01-06 00:49:43 -0500

    Creating structure for recyclerview

--- app/src/main/java/com/joebruzek/onerepmax/fragments/ListFragment.kt
+++ app/src/main/java/com/joebruzek/onerepmax/fragments/ListFragment.kt
@@ -1,12 +1,17 @@
 package com.joebruzek.onerepmax.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.joebruzek.onerepmax.MaxCalculator
+import com.joebruzek.onerepmax.OneRepMaxRecord
 import com.joebruzek.onerepmax.R
+import com.joebruzek.onerepmax.adapters.MaxListAdapter
 
 /*
  * The list fragment displays a list of one rep max values
@@ -14,6 +19,8 @@ import com.joebruzek.onerepmax.R
  * @author Joe Bruzek - 1/5/2018
  */
 class ListFragment : Fragment() {
+
+    private var listener: OnListFragmentInteractionListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,9 +31,39 @@ class ListFragment : Fragment() {
         // Set the adapter
         if (view is RecyclerView) {
             with(view) {
-                //TODO: set adapter and stuff
+                layoutManager = LinearLayoutManager(context)
+                adapter = MaxListAdapter(MaxCalculator.getMaxList(), listener)
             }
         }
         return view
+    }
+
+    /*
+     * attach to the activity and make sure the activity implements the listener interface
+     */
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
+        }
+
+    }
+
+    /*
+     * kill listener on detach
+     */
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /*
+     * Listener interface for the ListFragment. This allows us to send interaction information to the activity and
+     * handle it there
+     */
+    interface OnListFragmentInteractionListener {
+        fun onListFragmentInteraction(item: OneRepMaxRecord?)
     }
 }
