29d825f985fca1e3410637c9412a7dfd535e9733
Joe Bruzek <jbruzek@vt.edu>
2019-01-06 13:52:00 -0500

    started work on the chart

--- app/src/main/java/com/joebruzek/onerepmax/fragments/ChartFragment.kt
+++ app/src/main/java/com/joebruzek/onerepmax/fragments/ChartFragment.kt
@@ -1,11 +1,17 @@
 package com.joebruzek.onerepmax.fragments
 
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.joebruzek.onerepmax.MaxCalculator
 import com.joebruzek.onerepmax.R
 import com.joebruzek.onerepmax.viewmodels.ChartFragmentViewModel
 
@@ -41,7 +47,7 @@ class ChartFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         arguments?.let {
-            name = it.getString(ARG_NAME)
+            name = it.getString(ARG_NAME)!!
         }
     }
 
@@ -62,5 +68,43 @@ class ChartFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ChartFragmentViewModel::class.java)
         viewModel.name = name
+
+        val exercise = activity?.findViewById<TextView>(R.id.list_item_exercise_name)
+        val oneRM = activity?.findViewById<TextView>(R.id.list_item_max_weight)
+
+        exercise?.text = name
+        oneRM?.text = MaxCalculator.maxMap[name]?.toString()
+
+        initChart()
+    }
+
+    /*
+     * Initialize the chart and set the formatting
+     */
+    private fun initChart() {
+        val chart = activity?.findViewById<LineChart>(R.id.chart)
+        chart?.data = viewModel.getChartData()
+        chart?.lineData?.setDrawValues(false)
+
+        //X Axis
+        val xAxis = chart?.xAxis
+        xAxis?.granularity = 1f
+        xAxis?.valueFormatter = viewModel.getXAxisValueFormatter()
+        xAxis?.textColor = Color.parseColor("#BEFFFFFF")
+        xAxis?.textSize = 12f
+        xAxis?.position = XAxis.XAxisPosition.BOTTOM
+
+        //Y Axis
+        val right: YAxis? = chart?.axisRight
+        right?.isEnabled = false
+        val left: YAxis? = chart?.axisLeft
+        left?.textColor = Color.parseColor("#BEFFFFFF")
+        left?.textSize = 14f
+
+        //Legend
+        chart?.legend?.isEnabled = false
+        chart?.description?.isEnabled = false
+
+        chart?.invalidate()
     }
 }
