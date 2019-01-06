3cde1dc35853e06678bfd91014a0a504680c97a1
Joe Bruzek <jbruzek@vt.edu>
2019-01-05 23:35:08 -0500

    adding some helper methods that will be used to get all time max values

---
+++ app/src/main/java/com/joebruzek/onerepmax/OneRepMaxDataClasses.kt
@@ -0,0 +1,12 @@
+package com.joebruzek.onerepmax
+
+/*
+ * This file holds data classes pertaining to max values
+ */
+
+/*
+ * A record of an exercise and a max
+ *
+ * @author: Joe Bruzek 1/5/2018
+ */
+data class OneRepMaxRecord(val exercise: String, val max: Int)
